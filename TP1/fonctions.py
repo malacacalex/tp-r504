1b0458a735369eed4ba1c451fa668a9879eca0bf
malacacalex <malacaxel@gmail.com>
2023-09-06 16:58:49 +0200

    TP1 suite

---
+++ TP1/fonctions.py
@@ -0,0 +1,2 @@
+def puissance(a,b) :
+	return a**b
---
+++ TP1/prog1.py
@@ -0,0 +1,10 @@
+print ("Hello, World!")
+import fonctions as f
+
+while True :
+	x1=int(input("Entrez un nombre :"))
+	x2=int(input("Entrez un nombre :"))
+	res = f.puissance(x1,x2)
+	print (f"la puissance vaut {res} ")
+	
+
