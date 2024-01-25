b37e54141767e2e7893b33a051bf00b05b8bb8a8
malacalex <malacaxel@gmail.com>
2024-01-25 16:53:03 +0100

    tp9

--- TP9/ClientTCP2.java
+++ TP9/ClientTCP2.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-public class ClientTCP1
+public class ClientTCP2
 {
    public static void main( String[] args )
    {
@@ -8,7 +8,7 @@ public class ClientTCP1
    		{
 			Socket socket = new Socket( "localhost", 2016 );
 			DataOutputStream dOut = new DataOutputStream( socket.getOutputStream() );
-			dOut.writeUTF( "message test" );
+			dOut.writeUTF( args[0] );
 			socket.close();
     	}
    
--- TP9/ServeurTCP2.java
+++ TP9/ServeurTCP2.java
@@ -1,6 +1,6 @@
 import java.io . * ;
 import java.net . * ;
-public class ServeurTCP1
+public class ServeurTCP2
 {
 	public static void main ( String [ ] args )
 	{
@@ -9,13 +9,15 @@ public class ServeurTCP1
 			ServerSocket socketserver = new ServerSocket( 2016 );
 			System.out.println( "serveur en attente" );
 			Socket socket = socketserver.accept();
-			System.out.println( "Connection d'un client" );
-			DataInputStream dIn = new DataInputStream( socket.getInputStream());
-			System.out.println( "Message: " + dIn.readUTF());
-    		socket.close();
-			socketserver.close();
+			while(true)
+				{
+				System.out.println( "Connection d'un client" );
+				DataInputStream dIn = new DataInputStream( socket.getInputStream());
+				System.out.println( "Message: " + dIn.readUTF());
+    			socket.close();
+				socketserver.close();
 			}
-		
+		}
 		catch( Exception ex )
 		{
     		System.out.println( "erreur !");
