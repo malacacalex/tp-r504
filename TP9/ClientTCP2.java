0440876264afc94e8224e88278a4f6927b922fb6
malacalex <malacaxel@gmail.com>
2024-01-25 17:08:41 +0100

    ee

--- TP9/ClientTCP2.java
+++ TP9/ClientTCP2.java
@@ -1,9 +1,14 @@
 import java.io.*;
 import java.net.*;
+
 public class ClientTCP2
 {
    public static void main( String[] args )
    {
+		if (args.length < 1) {
+            System.out.println("Usage: java ClientTCP2 <message>");
+            return;
+		}
    		try
    		{
 			Socket socket = new Socket( "localhost", 2016 );
--- TP9/ServeurTCP2.java
+++ TP9/ServeurTCP2.java
@@ -8,14 +8,15 @@ public class ServeurTCP2
      	{
 			ServerSocket socketserver = new ServerSocket( 2016 );
 			System.out.println( "serveur en attente" );
-			Socket socket = socketserver.accept();
+
 			while(true)
-				{
+			{
+				Socket socket = socketserver.accept();
 				System.out.println( "Connection d'un client" );
 				DataInputStream dIn = new DataInputStream( socket.getInputStream());
 				System.out.println( "Message: " + dIn.readUTF());
     			socket.close();
-				socketserver.close();
+//				socketserver.close();
 			}
 		}
 		catch( Exception ex )
