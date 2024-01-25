65d0db734e1ce93fd6be5db9d73bfbc1b426d0fe
malacalex <malacaxel@gmail.com>
2024-01-25 17:21:19 +0100

    ee

---
+++ TP9/Clienthttp.java
@@ -0,0 +1,34 @@
+import java.io . * ;
+import java.net . * ;
+public class Clienthttp
+{
+	public static void main ( String [ ] args )
+	{
+	 	try
+     	{
+			OutputStreamWriter osw = new OutputStreamWriter( socket.getOutputStream() );
+			InputStreamReader isw = new InputStreamReader( socket.getInputStream() );
+
+			BufferedWriter bufOut = new BufferedWriter (osw);
+			BufferedReader bufIn = new BufferedReader (isw);
+			
+			String request = "GET / HTTP/1.0\r\n\r\n";
+			bufOut.write( request, 0, request.length() );
+			bufOut.flush();
+
+			String line = bufIn.readLine();
+			while( line != null ) {
+				System.out.println( line );
+				line = bufIn.readLine();
+			}
+			bufIn.close();
+			bufOut.close();
+			socket.close();			
+
+		}
+		catch( Exception ex )
+		{
+    		System.out.println( "erreur !");
+		}
+	}
+}	
